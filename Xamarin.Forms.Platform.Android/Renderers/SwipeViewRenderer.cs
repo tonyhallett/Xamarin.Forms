b39e9b70f59f646de53d3e47d002ae952a6a8e3b
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-09-13 16:31:21 +0200

    Fix crash in SwipeView on Android collapsing ListView groups

--- Xamarin.Forms.Platform.Android/Renderers/SwipeViewRenderer.cs
+++ Xamarin.Forms.Platform.Android/Renderers/SwipeViewRenderer.cs
@@ -377,8 +377,12 @@ namespace Xamarin.Forms.Platform.Android
 		{
 			if (_contentView != null)
 			{
-				_contentView.RemoveFromParent();
-				_contentView.Dispose();
+				if (!_contentView.IsDisposed())
+				{
+					_contentView.RemoveFromParent();
+					_contentView.Dispose();
+				}
+
 				_contentView = null;
 			}
 
@@ -387,6 +391,7 @@ namespace Xamarin.Forms.Platform.Android
 			else
 				_contentView = CreateContent();
 
+			_contentView.RemoveFromParent();
 			AddView(_contentView);
 
 			UpdateBackgroundColor();
